225bab190097a6633a7f2061a7ab45ad476d0858
Adrián Ferrera González <adrian.afergon@gmail.com>
2018-08-15 18:27:36 +0100

    test(App): [Red] Create test for find button

--- App.test.js
+++ App.test.js
@@ -2,8 +2,24 @@ import React from 'react';
 import App from './App';
 
 import renderer from 'react-test-renderer';
+import Adapter from 'enzyme-adapter-react-16'
+import { shallow, configure } from 'enzyme';
+
+configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered).toBeTruthy();
+});
+
+describe ('test', () => {
+  it('shallow', () => {
+    const element = shallow(<App/>);
+    expect(element.exists()).toBeTruthy();
+  });
+  it('should find button', () => {
+    const element = shallow(<App/>);
+    const aButton = element.find('Button');
+    expect(aButton.exists()).toBeTruthy();
+  });
 });
