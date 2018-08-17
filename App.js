6543196890a1877347f4d8fe8cacd878fde57329
Adrián Ferrera González <adrian.afergon@gmail.com>
2018-08-17 17:06:01 +0100

    feat(TextInput): Create text input

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default class App extends React.Component {
 
@@ -10,10 +10,11 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-          <Text>Changes you make will automatically reload.</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
-          <Button title='This is my button' onPress={this.handlerPress}/>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+        <Button title='This is my button' onPress={this.handlerPress}/>
+        <TextInput />
       </View>
     );
   }
